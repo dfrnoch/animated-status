25185c397e50138090478f0cc11f18f7274c5eed
LnX <in@lnx.rip>
2020-06-28 22:17:51 +0200

    Update animated.py

--- animated.py
+++ animated.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 #CONFIG
-token = "your-token" # Your Discord token
+token = "your-token" # Your Discord token (https://www.youtube.com/watch?v=co9TvsAtUmY)
 status = "Look! Animated Status!" # Text you want to be animated
 emoji = "😋" # Emoji like this: 😋 (Leave blank for none)
 speed = 0.9 # Speed of animating, recommended 0.5 - 1.5 seconds (I tried it for like half hour for 0.5s and its not ratelimiting or something)
