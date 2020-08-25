d678547872542e3304596f20fef4169fd0b30cbf
LnX <in@lnx.rip>
2020-08-25 12:57:56 +0200

    Update animated.py

--- animated.py
+++ animated.py
@@ -1,33 +1,37 @@
 import requests
 from time import sleep
+from colorama import Fore
+import json
+import os
+import emoji
 
+with open('config.json') as f:
+    config = json.load(f)
 
-#CONFIG
-token = "your-token" # Your Discord token (https://www.youtube.com/watch?v=co9TvsAtUmY)
-status = "Look! Animated Status!" # Text you want to be animated
-emoji = "😋" # Emoji like this: 😋 (Leave blank for none)
-speed = 0.9 # Speed of animating, recommended 0.5 - 1.5 seconds (I tried it for like half hour for 0.5s and its not ratelimiting or something)
+token = config.get("token")
 
+os.system("cls")
 
+status = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Text you want to display: ")
+print(f"\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Search here: {Fore.WHITE}www.webfx.com/tools/emoji-cheat-sheet {Fore.LIGHTBLACK_EX}(Leave empty if you dont want emoji)")
+emoji = emoji.emojize(input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Insert emoji name: "), use_aliases=True)
+print(f"\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Reccomended time is 0.5-1.5s")
+speed = float(input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Text speed: "))
 
-# Text
-print("")
-print("")
-print("Animating..")
-
+print(f"\n\n{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Animating..")
 
 
 
 # The full code
 while True:
-    for _i in range(0, len(status)+1):
+    for text in range(0, len(status)+1):
         if emoji != "":
             content = {
-                "custom_status": {"text": status[:_i],"emoji_name": emoji}
+                "custom_status": {"text": status[:text], "emoji_name": emoji}
             }
         else:
             content = {
-                "custom_status": {"text": status[:_i]}
+                "custom_status": {"text": status[:text]}
             }
         requests.patch("https://ptb.discordapp.com/api/v6/users/@me/settings", headers={"authorization": token}, json=content)
         sleep(speed)
