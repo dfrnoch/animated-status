74b818c9a26c24c3a08e62a3508ef5d671c511ef
LnX <in@lnx.rip>
2020-08-28 13:38:14 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @echo off
-title Animated Status Installer
+cls
+title Installer
 echo Installing..
-pip install -r requirements.txt
+py -3.8 -m pip install -r requirements.txt
 echo Succesfully Installed all requirements
 pause
